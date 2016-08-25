6f19a9a5b15fa3e0bf2667ec46b1d953eb67f4e0
Administrator <Administrator@C64-20160503BHE>
2016-08-25 18:19:02 +0800

    d

--- jsp1/src/test/SpringMVC/mvcController.java
+++ jsp1/src/test/SpringMVC/mvcController.java
@@ -1,17 +1,23 @@
 package test.SpringMVC;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
-import java.util.jar.Attributes.Name;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bean.MatchService;
 import com.entry.FixedIncome;
@@ -35,6 +41,22 @@ public class mvcController {
 		model.addAttribute("fixedIncomes", fixedIncomes);
 		return "n2";
 	}
+	
+	@ResponseBody 
+	@RequestMapping(value="/helloo{type}",produces="application/json;charset=UTF-8")
+	public String hello1(Model model,HttpServletRequest request,@PathVariable("type")String type) throws JsonGenerationException, JsonMappingException, IOException {
+		System.out.println(type);
+		int count=request.getSession().getAttribute("count")==null?0:Integer.parseInt(request.getSession().getAttribute("count").toString());
+		request.getSession().setAttribute("count", count+1);
+		List<FixedIncome> fixedIncomes=matchService.method3();
+		model.addAttribute("fixedIncomes", fixedIncomes);
+		Map<String, String> map=new HashMap<>();
+		map.put("fixed", "aa");
+		ObjectMapper mapper=new ObjectMapper();
+		String res=mapper.writeValueAsString(map);
+		return res;
+	}
+	
 	
 	@PostConstruct
 	private void cons(){
